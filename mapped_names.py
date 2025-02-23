80e9006690eec63da1b12ac6565e53fe00132af5
joeyb908 <3558837-joeyb908@users.noreply.gitlab.com>
2025-02-23 15:40:37 -0500

    added mapped names

---
+++ mapped_names.py
@@ -0,0 +1,11 @@
+hero_name_map = {
+    "Adam Warlock": "Adam", "Black Panther": "Panther",
+    "Black Widow": "Widow", "Captain America": "Cap",
+    "Cloak & Dagger": "Cloak", "Doctor Strange": "Strange",
+    "Invisible Woman": "Invis", "Jeff the Land Shark": "Jeff",
+    "Luna Snow": "Luna", "Mister Fantastic": "Mr F",
+    "Moon Knight": "Moon", "Peni Parker": "Peni",
+    "Rocket Raccoon": "Rocket", "Scarlet Witch": "Wanda",
+    "Squirrel Girl": "Squirrel", "The Punisher": "Punisher",
+    "Winter Soldier": "Bucky"
+}
