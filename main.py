f37527ea0a86570b13d06083cc01e2a6cc660190
joeyb908 <3558837-joeyb908@users.noreply.gitlab.com>
2025-02-22 23:13:00 -0500

    added name of target banned person

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ def check_profile(profile_name, ban_list):
         profile_hero_stats = hero_stats.HeroStats(heroes)
         profile_hero_stats.ban_calculator()
         for hero in profile_hero_stats.ban_heroes:
-            add_to_master(ban_list, hero[0], hero[1], hero[2])
+            add_to_master(ban_list, hero[0], hero[1], hero[2], profile_name)
         scraper.web_driver.driver.close()
     except KeyError:
         print(f"Unable to check {profile_name}'s profile...")
 
-def add_to_master(ban_list, hero_name, hero_winrate, hero_matches):
+def add_to_master(ban_list, hero_name, hero_winrate, hero_matches, profile_name):
     if len(ban_list) < MIN_BANLIST_SIZE:
-        ban_list.append([hero_name, hero_winrate, hero_matches])
+        ban_list.append([hero_name, hero_winrate, hero_matches, profile_name])
 
     else:
         min_value = min(ban_list, key=lambda x: x[1])
         if hero_winrate > min_value[1]:
             min_index = ban_list.index(min_value)
-            ban_list[min_index] = [hero_name, hero_winrate, hero_matches]
+            ban_list[min_index] = [hero_name, hero_winrate, hero_matches, profile_name]
 
 def check_profiles(list_of_profiles):
     for name in list_of_profiles:
@@ -42,7 +42,7 @@ check_profiles(profile_names)
 if len(master_ban_list) == 0:
     print("No heroes with high enough winrate to consider banning")
 else:
-    print(f"Top {len(master_ban_list)} heroes to ban")
+    print(f"Top {len(master_ban_list)} heroes to ban:")
     for i in range(len(master_ban_list)):
-          print(f"\t{master_ban_list[i][0]}: {round(master_ban_list[i][1])}% with {master_ban_list[i][2]} matches played")
+          print(f"\t{master_ban_list[i][0]}: {round(master_ban_list[i][1])}% with {master_ban_list[i][2]} matches played by {master_ban_list[i][3]}")
 
