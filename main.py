5b432556b6bce849ff2c24b4bd0782fc614a8120
joeyb908 <3558837-joeyb908@users.noreply.gitlab.com>
2025-02-23 01:30:21 -0500

    implemented multithreading of check_profiles()

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import page_scraper
 import hero_stats
 from screenshot_reader import image_to_string
+import threading
+import time
 
 MIN_BANLIST_SIZE = 10
+
+# Record the start time
+start = time.time()
 
 def check_profile(profile_name, ban_list):
     try:
@@ -28,9 +33,16 @@ def add_to_master(ban_list, hero_name, hero_winrate, hero_matches, profile_name)
             ban_list[min_index] = [hero_name, hero_winrate, hero_matches, profile_name]
 
 def check_profiles(list_of_profiles):
+    a = {}
+    k = 0
     for name in list_of_profiles:
-        print(f"Checking {name}...")
-        check_profile(name, master_ban_list)
+        a[k] = threading.Thread(target=check_profile, args=(name, master_ban_list))
+        a[k].start()
+        k += 1
+
+    for i in range(len(list_of_profiles)):
+        a[i].join()
+        print(f"Finished {list_of_profiles[i]}")
 
     master_ban_list.sort(key=lambda x: x[2], reverse=True)
 
@@ -38,10 +50,13 @@ profile_names = image_to_string()
 master_ban_list = []
 
 check_profiles(profile_names)
-
 if len(master_ban_list) == 0:
     print("No heroes with high enough winrate to consider banning")
 else:
     print(f"Top {len(master_ban_list)} heroes to ban:")
     for i in range(len(master_ban_list)):
           print(f"\t{master_ban_list[i][0]}: {round(master_ban_list[i][1])}% with {master_ban_list[i][2]} matches played by {master_ban_list[i][3]}")
+
+end = time.time()
+
+print(f"\nTotal time: {end - start} seconds")
